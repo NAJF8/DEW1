6daa88d72818df097681b7072935e7493753c84c
mohammedalhar <mohameadalhaear100@gmail.com>
2026-06-14 17:24:16 +0300

    Update admin password to 123456

--- config/admin.config.js
+++ config/admin.config.js
@@ -1,6 +1,6 @@
 export const ADMIN_CONFIG = {
   salt: 'dew-admin-2026',
-  passwordHash: '1d35435444ffae33f56255aece068d053cccd341846bbc70beab5319ddd9ef80',
+  passwordHash: 'afdb5296dc6d0cdc936175df8bdc132f83a81b62038a8298983dd4108081008f',
   sessionTokenPrefix: 'dew-admin',
   sessionTtlMs: 20 * 60 * 1000,
   idleLogoutMs: 15 * 60 * 1000,
